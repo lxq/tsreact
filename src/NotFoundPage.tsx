d27ff46c017b8f781236598ce2a040755f70f7f0
lxq <lxq@weetgo.com>
2019-07-22 20:08:58 +0800

    增加无效地址的页面。

---
+++ src/NotFoundPage.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+const NotFoundPage: React.SFC = () => {
+
+    return (
+        <div className="page-container">
+            <h1>抱歉，该页面不存！</h1>
+        </div>
+    );
+};
+
+export default NotFoundPage;
--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,21 +1,26 @@
 import * as React from "react";
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 import AdminPage from "./adminpage";
 import ProductsPage from "./productspage";
 import Header from "./header";
 import ProductPage from "./ProdectPage";
+import NotFoundPage from "./NotFoundPage";
 
 const Routes: React.SFC = () => {
     return (
         <Router>
             <div>
                 <Header />
-                {/* path属性对应URL，如果URL没有对应path路径则渲染的是空页面。 */}
-                {/* exact 属性控制组件是否呈现。 */}
-                <Route exact={true} path="/products" component={ProductsPage} />
-                <Route path="/products/:id" component={ProductPage} />
-                <Route path="/admin" component={AdminPage} />
+                {/* Switch 组件只渲染第一个匹配的。 */}
+                <Switch>
+                    {/* path属性对应URL，如果URL没有对应path路径则渲染的是空页面。 */}
+                    {/* exact 属性控制组件是否呈现。 */}
+                    <Route exact={true} path="/products" component={ProductsPage} />
+                    <Route path="/products/:id" component={ProductPage} />
+                    <Route path="/admin" component={AdminPage} />
+                    <Route component={NotFoundPage} />
+                </Switch>
             </div>
         </Router>
     );
