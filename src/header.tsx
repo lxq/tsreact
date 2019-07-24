97f62515c6be4766c66cb89c2005b20579681952
lxq <lxq@weetgo.com>
2019-07-24 10:41:38 +0800

    增加搜索框，传递URL参数。

--- src/header.tsx
+++ src/header.tsx
@@ -1,11 +1,35 @@
 import * as React from "react";
-import {NavLink} from "react-router-dom";
+import {NavLink, RouteComponentProps, withRouter} from "react-router-dom";
 
 import logo from "./logo.svg";
 
-const Header: React.SFC = () => {
+const Header: React.SFC<RouteComponentProps> = (props) => {
+    const [search, setSearch] = React.useState("");
+    React.useEffect(() => {
+        const prms = new URLSearchParams(props.location.search);
+        setSearch(prms.get("search") || "");
+    }, []);
+
+    const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSearch(e.currentTarget.value);
+    };
+    const handleSearchKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if ("Enter" === e.key) {
+            props.history.push(`/products?search=${search}`);
+        }
+    };
+
     return (
         <header className="header">
+            <div className="search-container">
+                <input
+                    type="search"
+                    placeholder="search"
+                    value={search}
+                    onChange={handleSearchChange}
+                    onKeyDown={handleSearchKeyDown} 
+                />
+            </div>
             <img src={logo} className="header-logo" alt="logo" />
             <h1 className="header-title">React商铺</h1>
             <nav>
@@ -16,4 +40,4 @@ const Header: React.SFC = () => {
     );
 };
 
-export default Header;
+export default withRouter(Header);
--- src/productspage.tsx
+++ src/productspage.tsx
@@ -1,19 +1,26 @@
 import * as React from "react";
-import {Link} from "react-router-dom";
+import {Link, RouteComponentProps} from "react-router-dom";
 
 import {IProduct, products} from "./productsdata";
 
 interface IState {
     products: IProduct[];
+    search: string;
 }
 
-class ProductsPage extends React.Component<{}, IState> {
+class ProductsPage extends React.Component<RouteComponentProps, IState> {
+    public static getDerivedStateFromProps(props:RouteComponentProps, state: IState) {
+        const prms = new URLSearchParams(props.location.search);
+        const search = prms.get("search") || "";
+        return {products: state.products, search};
+    }
 
-    public constructor(props: {}) {
+    public constructor(props: RouteComponentProps) {
         super(props);
 
         this.state = {
-            products: []
+            products: [],
+            search: ""
         };
     }
 
@@ -27,13 +34,20 @@ class ProductsPage extends React.Component<{}, IState> {
                 <p>欢迎来到我的React商铺！</p>
                 <ul className="product-list">
                     {/* 这里使用map函数进行迭代。 */}
-                    {this.state.products.map(product => (
-                        <li key={product.id} className="product-list-item">
-                            <Link to={`/products/${product.id}`}>
-                              {product.name}
-                            </Link>
-                        </li>
-                    ))}
+                    {this.state.products.map(product => {
+                        if (!this.state.search ||
+                             (this.state.search && product.name.toLowerCase().indexOf(this.state.search.toLowerCase())> -1)) {
+                                 return (
+                                    <li key={product.id} className="product-list-item">
+                                        <Link to={`/products/${product.id}`}>
+                                        {product.name}
+                                        </Link>
+                                    </li>        
+                                 );
+                        } else {
+                            return null;
+                        }
+                    })}
                 </ul>
             </div>
         );
